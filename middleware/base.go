1f14f641eed04eb7c34e668150d90ba288c9dd08
noah <noah@gmail.com>
2024-01-01 23:10:53 +0800

    feat:添加多个中间件

--- middleware/base.go
+++ middleware/base.go
@@ -73,6 +73,9 @@ func UniAuth(cfg redis.RedisConf) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			token := r.Header.Get("Authorization")
+			if token == "" {
+				return
+			}
 			key := fmt.Sprintf(consts.RedisKeyAuth, token)
 			exists, err := rds.Exists(key)
 			if err != nil || !exists {
