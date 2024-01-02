129a405db7a158209be178dc0102d34b807c9347
noah <noah@gmail.com>
2024-01-02 14:25:46 +0800

    feat:修改中间件

--- middleware/api_base.go
+++ middleware/api_base.go
@@ -3,15 +3,10 @@ package middleware
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/noahlsl/public/constants/consts"
-	"github.com/noahlsl/public/constants/enums"
-	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/go-zero/rest/httpx"
-	xerrors "github.com/zeromicro/x/errors"
-	xhttp "github.com/zeromicro/x/http"
 )
 
 func BaseMiddleware(next http.HandlerFunc) http.HandlerFunc {
@@ -64,26 +59,6 @@ func BaseCtxMiddleware(next http.HandlerFunc) http.HandlerFunc {
 		ctx = context.WithValue(ctx, "website", website)
 		ctx = context.WithValue(ctx, "debug", r.Header.Get("debug"))
 		r = r.WithContext(ctx)
-		next(w, r)
-	}
-}
-
-type UniAuth struct {
-	c redis.RedisConf
-}
-
-func (u *UniAuth) Handle(next http.HandlerFunc) http.HandlerFunc {
-	rds := redis.MustNewRedis(u.c)
-	return func(w http.ResponseWriter, r *http.Request) {
-		token := r.Header.Get("Authorization")
-		if token != "" {
-			key := fmt.Sprintf(consts.RedisKeyAuth, token)
-			exists, err := rds.Exists(key)
-			if err != nil || !exists {
-				xhttp.JsonBaseResponseCtx(r.Context(), w, xerrors.New(enums.ErrSysTokenExpired, "Login expired"))
-				return
-			}
-		}
 		next(w, r)
 	}
 }
---
+++ middleware/api_uni_auth.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"fmt"
+	"github.com/noahlsl/public/constants/consts"
+	"github.com/noahlsl/public/constants/enums"
+	"github.com/zeromicro/go-zero/core/stores/redis"
+	xerrors "github.com/zeromicro/x/errors"
+	xhttp "github.com/zeromicro/x/http"
+	"net/http"
+)
+
+type UniAuth struct {
+	c redis.RedisConf
+}
+
+func NewUniAuth(c redis.RedisConf) *UniAuth {
+	return &UniAuth{c: c}
+}
+
+func (u *UniAuth) Handle(next http.HandlerFunc) http.HandlerFunc {
+	rds := redis.MustNewRedis(u.c)
+	return func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		if token != "" {
+			key := fmt.Sprintf(consts.RedisKeyAuth, token)
+			exists, err := rds.Exists(key)
+			if err != nil || !exists {
+				xhttp.JsonBaseResponseCtx(r.Context(), w, xerrors.New(enums.ErrSysTokenExpired, "Login expired"))
+				return
+			}
+		}
+		next(w, r)
+	}
+}
