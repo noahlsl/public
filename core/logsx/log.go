177ea7b33a1f971f903017f3bd3ba74a81a54174
noah <noah@gmail.com>
2024-03-11 15:12:48 +0800

    fix:日志修改

--- core/logsx/log.go
+++ core/logsx/log.go
@@ -95,6 +95,7 @@ func InitLogX(c logx.LogConf) {
 }
 
 func MustSetUp(c logx.LogConf, fields ...zap.Field) *zap.Logger {
+	logx.MustSetup(c)
 	writer, logger := NewZapWriter(c, fields...)
 	logx.SetWriter(writer)
 	logger.Level()
