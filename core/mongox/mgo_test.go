238fdab85c11a9656ec9dd0cf604f3c198b99386
noah <noah9527@gmail.com>
2024-06-07 14:41:47 +0800

    feat:修改poolx

--- core/mongox/mgo_test.go
+++ core/mongox/mgo_test.go
@@ -1,6 +1,11 @@
 package mongox
 
-import "testing"
+import (
+	"context"
+	"github.com/noahlsl/public/helper/idx"
+	"testing"
+	"time"
+)
 
 func TestMgo(t *testing.T) {
 	cfg := &Cfg{
@@ -10,5 +15,33 @@ func TestMgo(t *testing.T) {
 		Password: "3QPEtoht7yXYRavp",
 		Database: "msg",
 	}
-	_ = cfg.NewDatabase()
+	c := cfg.NewDatabase()
+	v := NewPrivateMessage(1, 3206523318469632, 1, "hello")
+	one, err := c.Collection(cfg.Database).InsertOne(
+		context.TODO(), v)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(one)
+}
+
+type PrivateMessage struct {
+	ID         int64  `bson:"_id" json:"id"`
+	SenderID   int64  `bson:"sender_id" json:"sender_id"`
+	ReceiverID int64  `bson:"receiver_id" json:"receiver_id"`
+	Mode       int64  `bson:"mode" json:"mode"`
+	Content    string `bson:"content" json:"content"`
+	CreatedAt  int64  `bson:"created_at" json:"created_at"`
+}
+
+func NewPrivateMessage(senderID, receiverID, mode int64, content string) *PrivateMessage {
+	return &PrivateMessage{
+		ID:         idx.GenSnId(),
+		SenderID:   senderID,
+		ReceiverID: receiverID,
+		Mode:       mode,
+		Content:    content,
+		CreatedAt:  time.Now().UnixMilli(),
+	}
 }
