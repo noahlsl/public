e5eb3d6e29b44183c303efd0d0b29540b8733e3c
noah <noah9527@gmail.com>
2024-06-07 14:00:37 +0800

    feat:修改poolx

--- helper/poolx/pool.go
+++ helper/poolx/pool.go
@@ -17,3 +17,12 @@ func NewFnPool(fn func(in []byte), size ...int) *ants.PoolWithFunc {
 
 	return fnPool
 }
+
+func MustPoll(size int) *ants.Pool {
+	// 设置 ants 协程池的大小和其他选项 WithPreAlloc=true预先分配内存.初始浪费内存但是会减少GC压力
+	pool, err := ants.NewPool(size, ants.WithPreAlloc(true))
+	if err != nil {
+		panic(err)
+	}
+	return pool
+}
