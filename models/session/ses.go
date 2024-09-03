3d379364b6e06497ec0e407d6e42c46412ebd2ac
noah <noah9527@gmail.com>
2024-09-03 14:54:30 +0800

    feat:优化ses

--- models/session/ses.go
+++ models/session/ses.go
@@ -2,73 +2,100 @@ package session
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/noahlsl/public/constants/consts"
 	"github.com/noahlsl/public/helper/jwtx"
-	"github.com/pkg/errors"
 	"github.com/zeromicro/go-zero/core/stores/redis"
 )
 
-const (
-	AccessExpire = int64(3600 * 24)
-)
-
 type Ses struct {
-	r *redis.Redis
+	r      *redis.Redis
+	expire int64 // 过期时间秒
 }
 
-func NewSes(r *redis.Redis) *Ses {
+func NewSes(r *redis.Redis, expire int64) *Ses {
 	return &Ses{
-		r: r,
+		r:      r,
+		expire: expire,
 	}
 }
 
-func (s *Ses) Login(ctx context.Context, secret string, id string, ex ...int64) (string, error) {
-
-	accessExpire := AccessExpire
-	if len(ex) > 0 {
-		accessExpire = ex[0]
-	}
-	var token string
-	// 单点登录
-	key := fmt.Sprintf(consts.RedisKeyUid, id)
-	token, err := s.r.GetCtx(ctx, fmt.Sprintf(consts.RedisKeyUid, id))
-	if err != nil && !errors.Is(err, redis.Nil) {
-		return token, errors.WithStack(err)
-	}
-
-	if token != "" {
-		_, _ = s.r.DelCtx(ctx, fmt.Sprintf(consts.RedisKeyAuth, token))
-	}
+// Login 多点登录
+// values 选填参数填入登录信息,设备信息等的JSON字符串
+func (s *Ses) Login(ctx context.Context, secret string, id string, values ...any) (string, error) {
 
 	// 生成 jwt 响应
 	now := time.Now().Unix()
-	token, err = jwtx.GenJwtToken(secret, now, accessExpire, id)
+	token, err := jwtx.GenJwtToken(secret, now, s.expire, id)
 	if err != nil {
 		return "", err
 	}
 
-	key = fmt.Sprintf(consts.RedisKeyAuth, token)
-	err = s.r.SetexCtx(ctx, key, id, int(accessExpire))
+	key := fmt.Sprintf(consts.RedisKeyAuth, token)
+	err = s.r.SetexCtx(ctx, key, id, int(s.expire))
 	if err != nil {
 		return "", err
 	}
 
-	key = fmt.Sprintf(consts.RedisKeyUid, id)
-	err = s.r.SetexCtx(ctx, key, token, int(accessExpire))
+	key = fmt.Sprintf(consts.RedisKeyUidMap, id)
+	var val = id
+	if len(values) > 0 {
+		if v, ok := values[0].(string); ok {
+			val = v
+		}
+	}
+
+	err = s.r.HsetCtx(ctx, key, token, val)
 	if err != nil {
 		return "", err
 	}
 
+	_ = s.r.ExpireCtx(ctx, key, int(s.expire))
 	return token, nil
 }
 
-func (s *Ses) Logout(ctx context.Context, id string) error {
+// LoginOnce 单点登录
+// values 选填参数填入登录信息,设备信息等的JSON字符串
+func (s *Ses) LoginOnce(ctx context.Context, secret string, id string, values ...any) (string, error) {
+	// 单点登录
+	key := fmt.Sprintf(consts.RedisKeyUidMap, id)
+	_, _ = s.r.DelCtx(ctx, key)
 
-	key := fmt.Sprintf(consts.RedisKeyUid, id)
-	token, err := s.r.GetCtx(ctx, key)
+	// 生成 jwt 响应
+	now := time.Now().Unix()
+	token, err := jwtx.GenJwtToken(secret, now, s.expire, id)
+	if err != nil {
+		return "", err
+	}
+
+	tokenKey := fmt.Sprintf(consts.RedisKeyAuth, token)
+	err = s.r.SetexCtx(ctx, tokenKey, id, int(s.expire))
+	if err != nil {
+		return "", err
+	}
+
+	var val = id
+	if len(values) > 0 {
+		if v, ok := values[0].(string); ok {
+			val = v
+		}
+	}
+
+	err = s.r.HsetCtx(ctx, key, token, val)
+	if err != nil {
+		return "", err
+	}
+
+	_ = s.r.ExpireCtx(ctx, key, int(s.expire))
+	return token, nil
+}
+
+func (s *Ses) Logout(ctx context.Context, token string) error {
+	key := fmt.Sprintf(consts.RedisKeyAuth, token)
+	id, err := s.r.GetCtx(ctx, key)
 	if err != nil {
 		if errors.Is(err, redis.Nil) {
 			return nil
@@ -77,25 +104,12 @@ func (s *Ses) Logout(ctx context.Context, id string) error {
 		return err
 	}
 
-	err = s.r.PipelinedCtx(ctx, func(pipe redis.Pipeliner) error {
-		key = fmt.Sprintf(consts.RedisKeyAuth, token)
-		err = pipe.Del(ctx, key).Err()
-		if err != nil {
-			return err
-		}
-
-		key = fmt.Sprintf(consts.RedisKeyUid, id)
-		err = pipe.Del(ctx, key).Err()
-		if err != nil {
-			return err
-		}
-
-		return nil
-	})
-
+	_, err = s.r.DelCtx(ctx, key)
 	if err != nil {
 		return err
 	}
 
+	key = fmt.Sprintf(consts.RedisKeyUidMap, id)
+	_, _ = s.r.HdelCtx(ctx, key, token)
 	return nil
 }
