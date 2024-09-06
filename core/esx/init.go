3a414f367f47cedb9315977e0bb66b628a88e6bc
noah <noah9527@gmail.com>
2024-09-06 13:29:41 +0800

    feat:elastic

--- core/esx/init.go
+++ core/esx/init.go
@@ -18,6 +18,8 @@ func (c *Cfg) NewClient() *elastic.Client {
 	var opts []elastic.ClientOptionFunc
 	// 设置 Elasticsearch 服务器地址
 	opts = append(opts, elastic.SetURL(urls...))
+	opts = append(opts, elastic.SetSniff(false))       // 禁用 Sniff，防止在集群中查找其他节点
+	opts = append(opts, elastic.SetHealthcheck(false)) // 可选，禁用健康检查)
 	if c.Username != "" && c.Password != "" {
 		// 选填: 设置用户名和密码
 		opts = append(opts, elastic.SetBasicAuth(c.Username, c.Password))
