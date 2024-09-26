6f349da03ca080b92f73d11aa3c6e9a042632d63
noah <noah9527@gmail.com>
2024-09-26 21:10:06 +0800

    feat:add maxLogins

--- constants/consts/redis_key.go
+++ constants/consts/redis_key.go
@@ -1,9 +1,9 @@
 package consts
 
 const (
-	RedisKeyAuth      = "auth:%s"      // token 唯一校验
-	RedisKeyUid       = "uid:%v"       // uid 校验
-	RedisKeyUserLogin = "login:set:%v" // uid map校验
-	RedisKeyAdminRole = "admin:role"   // 账号角色-MAP
-	RedisKeyRolePrem  = "role:prem:%v" // 角色权限集合
+	RedisKeyAuth      = "auth:%s"       // token 唯一校验
+	RedisKeyUid       = "uid:%v"        // uid 校验
+	RedisKeyUserLogin = "login:set:%v"  // uid map校验
+	RedisKeyAdminRole = "admin:role:%v" // 账号角色-MAP
+	RedisKeyRolePrem  = "role:prem:%v"  // 角色权限集合
 )
